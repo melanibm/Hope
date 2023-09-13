dbd78e173f3fc16be05910f20ebc2f9726019dff
melanibm <melanimoran.mm@gmail.com>
2023-09-13 19:26:03 -0300

    arreglos en javascript

--- main/tiendaonline.js
+++ main/tiendaonline.js
@@ -8,26 +8,29 @@ const COSTOENVIO = 500
 
 function compra(productos) {
     switch (productos.trim().toLowerCase()) {
-        case "BODY":
-            return body;
-        case "BODY10":
-            return body10;
-        case "ACCESORIOS":
-            return accesorios;
+        case "body":
+            return BODY;
+        case "body10":
+            return BODY10;
+        case "accesorios":
+            return ACCESORIOS;
+        default: 
+            return 0;
 
     }
 }
 
 // Ingeso de productos seleccionados
+alert("Bienvenido, Nuestros productos son: body, Body10, Accesorios")
 let productos = prompt("¿Que producto desea comprar?")
 let total = 0
 let conteo = 0
 //revisar porque no aparece el total
 //ingreso de màs de un producto 
-while ((productos.trim().toUpperCase() !== "FINALIZAR") && (productos !== " ")) {
+while ((productos.trim().toLowerCase() !== "finalizar") && (productos.trim() !== "")) {
     total += compra(productos);
     conteo++;
-    console.log("El producto" + conteo + "ingresado es" + productos + " y va un total de: " + total);
+    console.log("El producto #" + conteo + "ingresado es" + productos + " y va un total de: " + total);
     productos = prompt("¿Que producto desea comprar?")
 }
 
@@ -40,10 +43,11 @@ let celular
 if (envio.trim().toUpperCase() == "SI") {
     direccion = prompt("Ingrese la direccion de Envio")
     celular = prompt("Ingrese su numero de Celular")
-    console.log("El pedido tiene valor total de: " + total + " + el envio", "la direccion de envio es: " + direccion + "Su celular para coordinar es " + celular)
+    console.log("El pedido tiene valor total de: " + (total + COSTOENVIO) + "la direccion de envio es: " + direccion + "Su celular para coordinar es " + celular)
     total += COSTOENVIO
     alert("Gracias por tu compra! El total a pagar es: " + total)
 } else {
     console.log("El pedido tiene un precio total de: " + total + "Retira en la tienda")
-    alert("Gracias por tu compra! Podes retirar tu pedido a partir de las 24hs habiles")
+    alert("Gracais por su compra su total es: " + total)
+    alert("Podras retirar tu pedido a partir de las 24hs habiles")
 }
